64f352e46a6cfaa4c8a2de66d7ab031b9a99acb7
Bipra roy <biprakanti@yottolabs.com>
2018-02-23 21:46:22 +0530

    1.Simple admin user creation done.2.Signin-Signout without cache done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
 	def loggin
-		@user = User.new		
+		@users = User.all
+		puts @users.inspect
+		@user = User.find_by(username: params[:username])
+		if @user.present?
+			redirect_to dashboard_index_path
+		else
+			render 'new_loggin'
+		end		
 	end
 	def new_loggin
 		#New login form
 		@user = User.new
+	end
+	def new_admin
+		@user = User.new
+	end
+	def create_admin
+		@user = User.new(user_param)
+		if @user.save
+			render 'new_loggin'
+		else
+			render 'new_admin'
+		end
+	end
+	private
+	def user_param
+		params.require(:user).permit(:username,:password)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   get 'landing/index'
   root 'landing#index'
   resources :users
+
+  #Loggin to the user
   post 'loggin', to: 'users#loggin', as: :loggin_user
   get  'new_loggin', to: 'users#new_loggin', as: :new_loggin_user
+
+  #Create admin user
+  get 'new_admin',to: 'users#new_admin',as: :new_admin
+  post 'create_admin',to: 'users#create_admin',as: :create_admin
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
